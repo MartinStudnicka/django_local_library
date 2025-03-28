7cbe9ca4149fbf2e17e49bff40429e39b4e4414b
Martinstudnicka <koumes3@seznam.cz>
2025-03-28 20:39:33 +0100

    Zjednodušení mapování

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #Přidání nové applikace
-    'catalog.apps.CatalogConfig',
+    # 'catalog.apps.CatalogConfig',
+    'catalog',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
